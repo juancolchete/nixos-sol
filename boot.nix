8d19273263f942528122a9e7ee30e892daf6d81f
Juan Colchete <38388534+juancolchete@users.noreply.github.com>
2024-12-15 16:34:51 -0300

    fix: boot variables

--- boot.nix
+++ boot.nix
@@ -1,2 +1,4 @@
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+{
+boot.loader.systemd-boot.enable = true;
+boot.loader.efi.canTouchEfiVariables = true;
+}
