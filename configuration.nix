7f06fa66d91dbe8fd808dd32d817c4a48229ebf0
Juan Colchete <38388534+juancolchete@users.noreply.github.com>
2024-12-15 22:32:31 -0300

    chore: fix trouble on vars

--- configuration.nix
+++ configuration.nix
@@ -86,21 +86,21 @@ in {
   system.stateVersion = "24.11";
   system.activationScripts.rustup = ''
     echo $user 
-    PATH=${pkgs.rustup}/bin:/home/vars.user/.cargo/bin:${pkgs.curl}/bin:${pkgs.bash}/bin:run/current-system/sw/bin:/run/current-system/sw/bin/tar:/run/current-system/sw/bin/clang:$PATH
-    touch /home/vars.user/.bashrc 
-    rm /home/vars.user/.bashrc
-    touch /home/vars.user/.bashrc 
-    echo export PATH=${pkgs.solana-cli}:/home/vars.user/programs/bin:'$PATH' >> /home/vars.user/.bashrc
-    echo export LIBCLANG_PATH=${pkgs.llvmPackages.libclang.lib}/lib >> /home/vars.user/.bashrc
-    echo export LLVM_CONFIG_PATH=${pkgs.llvm}/bin/llvm-config/bin/llvm-config >> /home/vars.user/.bashrc
+    PATH=${pkgs.rustup}/bin:/home//${vars.user}/.cargo/bin:${pkgs.curl}/bin:${pkgs.bash}/bin:run/current-system/sw/bin:/run/current-system/sw/bin/tar:/run/current-system/sw/bin/clang:$PATH
+    touch /home/${vars.user}/.bashrc 
+    rm /home/${vars.user}/.bashrc
+    touch /home/${vars.user}/.bashrc 
+    echo export PATH=${pkgs.solana-cli}:/home/${vars.user}/programs/bin:'$PATH' >> /home/${vars.user}/.bashrc
+    echo export LIBCLANG_PATH=${pkgs.llvmPackages.libclang.lib}/lib >> /home/${vars.user}/.bashrc
+    echo export LLVM_CONFIG_PATH=${pkgs.llvm}/bin/llvm-config/bin/llvm-config >> /home/${vars.user}/.bashrc
     echo ${pkgs.systemd.dev}
-    echo export PKG_CONFIG_PATH=${pkgs.systemd.dev}/lib/pkgconfig >> /home/vars.user/.bashrc
-    echo export CFLAGS="-I${pkgs.systemd.dev}/include" >> /home/vars.user/.bashrc
-    echo export LDFLAGS="-L${pkgs.systemd.dev}/lib" >> /home/vars.user/.bashrc
-    echo export CC=/run/current-system/sw/bin/clang >> /home/vars.user/.bashrc
-    echo export NIXPKGS_ALLOW_UNFREE=1 >> /home/vars.user/.bashrc
-    echo alias build-sbf=cargo-build-sbf >> /home/vars.user/.bashrc
-    source /home/vars.user/.bashrc
+    echo export PKG_CONFIG_PATH=${pkgs.systemd.dev}/lib/pkgconfig >> /home/${vars.user}/.bashrc
+    echo export CFLAGS="-I${pkgs.systemd.dev}/include" >> /home/${vars.user}/.bashrc
+    echo export LDFLAGS="-L${pkgs.systemd.dev}/lib" >> /home/${vars.user}/.bashrc
+    echo export CC=/run/current-system/sw/bin/clang >> /home/${vars.user}/.bashrc
+    echo export NIXPKGS_ALLOW_UNFREE=1 >> /home/${vars.user}/.bashrc
+    echo alias build-sbf=cargo-build-sbf >> /home/${vars.user}/.bashrc
+    source /home/${vars.user}/.bashrc
     alias build-sbf=cargo-build-sbf
   '';
 
